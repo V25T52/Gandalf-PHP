8f1fc8227542c0a8ce18f5993d60d9cf4b25df6f
Nicolas <coulange.n@evogue.fr>
2016-12-06 10:03:20 +0100

    jour 2

--- include/index.php
+++ include/index.php
@@ -1,1 +1,13 @@
-/coursPHP/include/index.php
+<?php
+include('fichier2.php');
+include_once('fichier2.php');
+
+require('fichier2.php');
+require_once('fichier2.php');
+?>
+
+_once --> on n'importe le fichier une seule fois.
+
+include --> si le fichier n'est pas trouvé -> WARNING and go on
+
+require --> si le fichier n'est pas trouvé -> FATAL ERROR and stop
